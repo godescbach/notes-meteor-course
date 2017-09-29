4752409480e832fbf6aaa0f2278a1d76c376d6ce
tmostyn <tmostyn@zyston.com>
2017-09-29 13:12:10 -0500

    Added code to list and test notes.

--- imports/ui/NoteList.js
+++ imports/ui/NoteList.js
@@ -4,12 +4,18 @@ import { createContainer } from 'meteor/react-meteor-data';
 
 import { Notes } from '../api/notes';
 import NoteListHeader from './NoteListHeader';
+import NoteListItem from './NoteListItem';
 
 export const NoteList = (props) => {
   return (
     <div>
       NoteList { props.notes.length }
       <NoteListHeader />
+      { props.notes.map((note) => {
+        return (
+          <NoteListItem key={note._id} note={note} />
+        );
+      })}
     </div>
   );
 };
