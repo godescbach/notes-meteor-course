7b6a7304a991b5d7fba3b75b13fefa82dd46fe03
tmostyn <tmostyn@zyston.com>
2018-01-04 06:18:54 -0600

    Add test for failed insert.

--- imports/ui/NoteList.test.js
+++ imports/ui/NoteList.test.js
@@ -14,7 +14,7 @@ if (Meteor.isClient) {
       expect(wrapper.find('NoteListItem').length).toBe(2);
       expect(wrapper.find('NoteListEmptyItem').length).toBe(0);
     });
-    it('shoud render NoteListEmptyItem if zero notes', function () {
+    it('should render NoteListEmptyItem if zero notes', function () {
       const wrapper = mount(<NoteList notes={[]} />);
       
             expect(wrapper.find('NoteListItem').length).toBe(0);
